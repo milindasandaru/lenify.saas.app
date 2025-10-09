b03b7cdb69f05756ab3e559722e9b48f19283218
milindasandaru <sams.senarath@gmail.com>
2025-10-09 13:14:13 +0530

    implement companion table

--- components/companionList.tsx
+++ components/companionList.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { 
+import {
     Table,
     TableBody,
     TableCaption,
@@ -7,7 +7,7 @@ import {
     TableHead,
     TableHeader,
     TableRow
- } from '@/components/ui/table'
+} from '@/components/ui/table'
 import Link from 'next/dist/client/link';
 
 interface CompanionCardProps {
@@ -16,39 +16,50 @@ interface CompanionCardProps {
     className?: string;
 }
 
-const companionList = ({ title, companions, className}: CompanionCardProps) => {
-  return (
-    <article className={`p-4 border border-black rounded-lg shadow-md ${className}`}>
-        <h2 className='font-bold text-2xl'>Recent Sessions</h2>
+const companionList = ({ title, companions, className }: CompanionCardProps) => {
+    return (
+        <article className={`p-4 border border-black rounded-3xl shadow-md ${className}`}>
+            <h2 className='font-bold text-2xl'>Recent Sessions</h2>
 
-        <Table className='mt-4'>
-            <TableHeader className='bg-gray-200'>
-                <TableRow className='text-left'>
-                    <TableHead className='text-lg w-2/3'>Invoice</TableHead>
-                    <TableHead className='text-lg'>Status</TableHead>
-                    <TableHead className='text-lg'>Method</TableHead>
-                    <TableHead className='text-right text-lg'>Amount</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {companions ? companions.map(({id, subject, name, topic, duration}) => (
-                <TableRow>
-                    <TableCell>
-                        <Link href={`/companions/${id}`} className='font-medium'>
-                            <div className="flex item-center gap-2">
-                                <div className="size-[36px] flex items-center justify-center rounded-lg max-md:hidden">
-                                    <img src={`/icons/${subject}.svg`} alt="" />
+            <Table className='mt-4'>
+                <TableHeader className='bg-gray-200'>
+                    <TableRow className='text-left'>
+                        <TableHead className='text-lg w-2/3'>Invoice</TableHead>
+                        <TableHead className='text-lg'>Status</TableHead>
+                        <TableHead className='text-lg'>Method</TableHead>
+                        <TableHead className='text-right text-lg'>Amount</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {companions ? companions.map(({ id, subject, name, topic, duration }) => (
+                        <TableRow key={id}>
+                            <TableCell>
+                                <Link href={`/companions/${id}`} className='font-medium'>
+                                    <div className="flex item-center gap-2">
+                                        <div className="size-[36px] flex items-center justify-center rounded-lg max-md:hidden">
+                                            <img src={`/icons/${subject}.svg`} alt="" />
+                                        </div>
+                                        <div className="flex flex-col gap-2">
+                                            <p className='font-semibold text'>{name}</p>
+                                            <p className='text-sm text-gray-500'>{topic}</p>
+                                            <p className='text-sm text-gray-500'>{duration} min</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </TableCell>
+                            <TableCell>
+                                <div className="subject-badge w-fit max-md:hidden">
+                                    {subject}
                                 </div>
-                            </div>
-                        </Link>
-                    </TableCell>
-                </TableRow>
-                )) : null}
-            </TableBody>
-        </Table>
-    </article>
+                                <div className=""></div>
+                            </TableCell>
+                        </TableRow>
+                    )) : null}
+                </TableBody>
+            </Table>
+        </article>
 
-  )
+    )
 }
 
 export default companionList
