5dcf85162c97908a2139f79eff0fb5729195a94c
milindasandaru <sams.senarath@gmail.com>
2025-10-09 20:30:18 +0530

    implement companion form file

--- components/companionForm.tsx
+++ components/companionForm.tsx
@@ -1,12 +1,29 @@
 "use client"
 
 import React from 'react'
-import { z } from 'zod'
+import { minLength, z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
+import { Button } from '@/components/ui/button'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormMessage,
+    FormField,
+    FormItem,
+    FormLabel,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
 const formSchema = z.object({
-    name: z.string().min(2).max(50),
+  name: z.string().min(1, { message: 'Companion is required.'}),
+  subject: z.string().min(1, { message: 'Subject is required.'}),
+  topic: z.string().min(1, { message: 'Topic is required.'}),
+  duration: z.number().min(1, { message: 'Duration is required.'}),
+  voice: z.string().optional(),
+  style: z.string().optional(),
 })
 
 const CompanionForm = () => {
@@ -14,10 +31,15 @@ const CompanionForm = () => {
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: '',
+            subject: '',
+            topic: '',
+            duration: 15,
+            voice: '',
+            style: '',
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
         console.log(values)
     }
 }
