1bfaf476ba39d48239af862a9d920feef6cb7ba6
milindasandaru <sams.senarath@gmail.com>
2025-10-09 22:01:53 +0530

    implement user login and authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Navbar from "@/components/navbar";
 
 const bricolage = Bricolage_Grotesque({
@@ -19,11 +27,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
+          <Navbar />
+          <header className="flex gap-2 p-2">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
