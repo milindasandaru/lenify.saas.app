4d0f70e5da78ca6c3b0ddd77e636040a8cca386f
milindasandaru <sams.senarath@gmail.com>
2025-10-11 12:35:21 +0530

    implement companion.action.ts file

--- app/Companions/new/page.tsx
+++ app/Companions/new/page.tsx
@@ -1,7 +1,13 @@
 import CompanionForm from '@/components/companionForm'
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/dist/client/components/navigation';
 import React from 'react'
 
-const NewCompanion = () => {
+const NewCompanion = async () => { 
+  const { userId } = await auth();
+  if(!userId) {
+    redirect('/sign-in?redirect_url=/companions/new');
+  }
   return (
     <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
       <article>
