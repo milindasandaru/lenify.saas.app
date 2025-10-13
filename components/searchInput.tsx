98d0c212a0043677a72ac9777c41b6ced865f9ff
milindasandaru <sams.senarath@gmail.com>
2025-10-13 23:36:21 +0530

    Implement searchInput Page

--- components/searchInput.tsx
+++ components/searchInput.tsx
@@ -21,7 +21,7 @@ const SearchInput = () => {
 
   return (
     <div className='relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit'>
-      <img src="@/icon/search.svg" alt="search" width={15} height={15} />
+      <img src="/icons/search.svg" alt="search" width={15} height={15} />
         <input 
             placeholder='Search companions...'
             className='outline-none border-none bg-transparent'
