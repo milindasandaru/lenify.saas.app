7c36ea38aa1c1994a66bdb6122b7ae1c755fadba
milindasandaru <sams.senarath@gmail.com>
2025-10-13 19:56:50 +0530

    Implement Companion Page

--- app/Companions/page.tsx
+++ app/Companions/page.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { getAllCompanions } from '@/lib/actions/companion.action'
+import CompanionCard from '@/components/companionCard';
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+  const companions = await getAllCompanions({ subject, topic });
+
+  console.log(companions)
+
   return (
-    <div>
-      
-    </div>
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">Filters</div>
+      </section>
+      <section className='companions-grid'>
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} />
+        ))}
+
+      </section>
+    </main>
   )
 }
 
