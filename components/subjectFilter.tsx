e3c3635fcb1afd938bacfce253a64984b06cf8e0
milindasandaru <sams.senarath@gmail.com>
2025-10-14 20:27:47 +0530

    Implement subjectFilter file

--- components/subjectFilter.tsx
+++ components/subjectFilter.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+'use client'
 
-const subjectFilter = () => {
+import { subjects } from '@/constants'
+import { removeKeysFromUrlQuery } from '@jsmastery/utils'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@radix-ui/react-select'
+import { useRouter, useSearchParams } from 'next/dist/client/components/navigation'
+import React, { useEffect } from 'react'
+
+const SubjectFilter = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const query = searchParams.get('subject') || '';
+
+    const [subject, setsubject] = React.useState(query);
+
+    useEffect(() => {
+        let newURL = "";
+        if (subject=="all") {
+            newURL = removeKeysFromUrlQuery({
+                params: searchParams.toString(),
+                key: 'subject'
+            });
+        } else {
+            newURL = fromUrlQuery({
+                params: searchParams.toString(),
+                key: "subject",
+                value: subject,
+            });
+        }
+
+        router.push(newURL);
+    }, [subject]);
+
   return (
-    <div>
-      
-    </div>
+    <Select onValueChange={setsubject} value={subject}>
+        <SelectTrigger className=' border border-black rounded-lg px-2 py-1 h-fit input capitalize'>
+            <SelectValue placeholder="Select Subject" />
+        </SelectTrigger>
+        <SelectContent>
+            {subjects.map((subject) => (
+                <SelectItem key={subject} value={subject} className='capitalize'>
+                    {subject}
+                </SelectItem>
+            ))}
+        </SelectContent>
+    </Select>
+
   )
 }
 
-export default subjectFilter
+export default SubjectFilter
+function fromUrlQuery(arg0: { params: string; key: string; value: string }): string {
+    throw new Error('Function not implemented.')
+}
+
