45b1de4f92343cbb5e54571e215d9c6d1c41237b
milindasandaru <sams.senarath@gmail.com>
2025-10-18 21:04:41 +0530

    Implement CompaniionComponent.tsx

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -5,10 +5,10 @@ import { cn } from '@/lib/utils';
 import { vapi } from '@/lib/vapi.sdk';
 
 enum CallStatus {
-  INACTIVE = 'inactive',
-  CONNECTING = 'connecting',
-  ACTIVE = 'active',
-  FINISHED = 'finished',
+    INACTIVE = 'inactive',
+    CONNECTING = 'connecting',
+    ACTIVE = 'active',
+    FINISHED = 'finished',
 }
 
 const CompanionComponent = ({ companionId, subject, topic, userName, userImage, voice }: CompanionComponentProps) => {
@@ -16,12 +16,12 @@ const CompanionComponent = ({ companionId, subject, topic, userName, userImage, 
     const [speechStatus, setSpeechStatus] = useState(false);
 
     useState(() => {
-      // Logic to handle companion interaction based on props  
+        // Logic to handle companion interaction based on props  
         const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
 
         const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
 
-        const onMessageReceive = (message: string) => {}
+        const onMessageReceive = (message: string) => { }
 
         const onSpeechStart = () => setSpeechStatus(true);
         const onSpeechEnd = () => setSpeechStatus(false);
@@ -30,26 +30,35 @@ const CompanionComponent = ({ companionId, subject, topic, userName, userImage, 
 
         vapi.on('call-start', onCallStart);
         vapi.on('call-end', onCallEnd);
-        vapi.on('message-receive', onMessageReceive);
+        vapi.on('message', onMessageReceive);
         vapi.on('error', onError);
         vapi.on('speech-start', onSpeechStart);
         vapi.on('speech-end', onSpeechEnd);
 
-    }, [companionId, subject, topic, userName, userImage, voice]);
+        return () => {
+            vapi.off('call-start', onCallStart);
+            vapi.off('call-end', onCallEnd);
+            vapi.off('message', onMessageReceive);
+            vapi.off('error', onError);
+            vapi.off('speech-start', onSpeechStart);
+            vapi.off('speech-end', onSpeechEnd);
+        }
 
-  return (
-    <section className='flex flex-col h-[70vh]'>
-        <section className='flex gap-8 max:sm:flex-col'>
-            <div className="companion-section">
-                <div className="companion-avatar">
-                    <div className={cn('absolute transition-opacity duration-1000')}>
+    }, /*[companionId, subject, topic, userName, userImage, voice]*/);
 
+    return (
+        <section className='flex flex-col h-[70vh]'>
+            <section className='flex gap-8 max:sm:flex-col'>
+                <div className="companion-section">
+                    <div className="companion-avatar">
+                        <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE ? 'opacity-100' : 'opacity-0', callStatus === CallStatus.CONNECTING ? 'opacity-100 animate-pulse' : '')}>
+                            <img src={`/icons/${subject}.svg`} alt={subject} width={70} height={70} className='max:sm:w-fit' />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
         </section>
-    </section>
-  )
+    )
 }
 
 export default CompanionComponent
